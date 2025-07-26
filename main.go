af63d567400b593a69baa2641c55ecc1b06d503c
Manish Pandey <manishmaang@gmail.com>
2025-07-26 16:49:44 +0530

    created separated folder for routes, will work on controllers now

--- main.go
+++ main.go
@@ -1,2 +1,20 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/manishmaang/TODO-APPLICATION/routes" 
+)
+
+func main() {
+	router := gin.Default()
+	// Hello world program !!
+	// router.GET("/", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "Hello world !!",
+	// 	})
+	// });
+
+	routes.SetupRoutes(router) // Register all routesroutes.SetupRoutes(router);
+
+	router.Run(":3000")
+}
